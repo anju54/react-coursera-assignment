6db11381f23a2928f62cde997b328a513d78f1f8
anju54 <anju.k302@gmail.com>
2020-02-27 00:00:56 +0530

    First commit

---
+++ src/shared/dishes.js
@@ -0,0 +1,39 @@
+export const DISHES =  [
+        {
+            id : 0,
+            name : 'Uthappizza',
+            image : 'assets/images/uthappizza.png',
+            category : 'mains',
+            label : 'Hot',
+            price : '4.99',
+            description : 'A unique combination of indian food and italian food'
+        },
+        {
+            id : 1,
+            name : 'Zucchipakoda',
+            image : 'assets/images/zucchipakoda.png',
+            category : 'appetizer',
+            label : '',
+            price : '1.99',
+            description : 'Deep fried zucinni coated with ....'
+        },
+        {
+            id : 2,
+            name : 'Elaicheese cake',
+            image : 'assets/images/elaicheesecake.png',
+            category : 'dessert',
+            label : 'Hot',
+            price : '2.99',
+            description : 'A Deletable semi sweet......'
+
+        },
+        {
+            id:3,
+            name : 'Vadonut',
+            image : 'assets/images/vadonut.png',
+            category : 'SouthIndian',
+            label : '',
+            price : '1.99',
+            description : 'A quintessential ConFusion experiance, is it vada or is it a donut?'
+        }
+];
